ab489fce3f56c81be4673b10753ba6df8edfd2a8
Jewoo Lee <shema7k@gmail.com>
2018-01-13 13:58:07 +0900

    Add tests for parsing Op

--- src/op.rs
+++ src/op.rs
@@ -92,3 +92,140 @@ impl fmt::Display for Op {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Op;
+    
+    #[test]
+    fn parse_add() {
+        let test_cases = [
+            ("+1", Ok(Op::Add(1))),
+            ("+12", Ok(Op::Add(12))),
+            ("+123", Ok(Op::Add(123))),
+            ("+1234", Ok(Op::Add(1234))),
+            ("+12345", Ok(Op::Add(12345))),
+            ("+123456", Ok(Op::Add(123456))),
+            ("+1234567", Ok(Op::Add(1234567))),
+        ];
+        
+        for &(ref input, ref output) in &test_cases {
+            assert_eq!(input.parse::<Op>(), *output);
+        }
+    }
+    
+    #[test]
+    fn parse_sub() {
+        let test_cases = [
+            ("-1", Ok(Op::Add(-1))),
+            ("-12", Ok(Op::Add(-12))),
+            ("-123", Ok(Op::Add(-123))),
+            ("-1234", Ok(Op::Add(-1234))),
+            ("-12345", Ok(Op::Add(-12345))),
+            ("-123456", Ok(Op::Add(-123456))),
+        ];
+        
+        for &(ref input, ref output) in &test_cases {
+            assert_eq!(input.parse::<Op>(), *output);
+        }
+    }
+    
+    #[test]
+    fn parse_mul() {
+        let test_cases = [
+            ("*1", Ok(Op::Mul(1))),
+            ("*12", Ok(Op::Mul(12))),
+            ("*123", Ok(Op::Mul(123))),
+            ("*1234", Ok(Op::Mul(1234))),
+            ("*12345", Ok(Op::Mul(12345))),
+            ("*123456", Ok(Op::Mul(123456))),
+            ("*1234567", Ok(Op::Mul(1234567))),
+            ("*-1", Ok(Op::Mul(-1))),
+            ("*-12", Ok(Op::Mul(-12))),
+            ("*-123", Ok(Op::Mul(-123))),
+            ("*-1234", Ok(Op::Mul(-1234))),
+            ("*-12345", Ok(Op::Mul(-12345))),
+            ("*-123456", Ok(Op::Mul(-123456))),
+        ];
+        
+        for &(ref input, ref output) in &test_cases {
+            assert_eq!(input.parse::<Op>(), *output);
+        }
+    }
+    
+    #[test]
+    fn parse_div() {
+        let test_cases = [
+            ("/1", Ok(Op::Div(1))),
+            ("/12", Ok(Op::Div(12))),
+            ("/123", Ok(Op::Div(123))),
+            ("/1234", Ok(Op::Div(1234))),
+            ("/12345", Ok(Op::Div(12345))),
+            ("/123456", Ok(Op::Div(123456))),
+            ("/1234567", Ok(Op::Div(1234567))),
+            ("/-1", Ok(Op::Div(-1))),
+            ("/-12", Ok(Op::Div(-12))),
+            ("/-123", Ok(Op::Div(-123))),
+            ("/-1234", Ok(Op::Div(-1234))),
+            ("/-12345", Ok(Op::Div(-12345))),
+            ("/-123456", Ok(Op::Div(-123456))),
+        ];
+        
+        for &(ref input, ref output) in &test_cases {
+            assert_eq!(input.parse::<Op>(), *output);
+        }
+    }
+    
+    #[test]
+    fn parse_del() {
+        assert_eq!("<<".parse::<Op>(), Ok(Op::Del));
+    }
+    
+    #[test]
+    fn parse_ins() {
+        let test_cases = [
+            ("1", Ok(Op::Ins(1))),
+            ("12", Ok(Op::Ins(12))),
+            ("123", Ok(Op::Ins(123))),
+            ("1234", Ok(Op::Ins(1234))),
+            ("12345", Ok(Op::Ins(12345))),
+            ("123456", Ok(Op::Ins(123456))),
+            ("1234567", Ok(Op::Ins(1234567))),
+        ];
+        
+        for &(ref input, ref output) in &test_cases {
+            assert_eq!(input.parse::<Op>(), *output);
+        }
+    }
+    
+    #[test]
+    fn parse_rpc() {
+        let test_cases = [
+            ("1=>2", Ok(Op::Rpc("1".into(), "2".into()))),
+            ("12=>3", Ok(Op::Rpc("12".into(), "3".into()))),
+            ("123=>45", Ok(Op::Rpc("123".into(), "45".into()))),
+            ("1234=>567", Ok(Op::Rpc("1234".into(), "567".into()))),
+        ];
+        
+        for &(ref input, ref output) in &test_cases {
+            assert_eq!(input.parse::<Op>(), *output);
+        }
+    }
+    
+    #[test]
+    fn parse_err() {
+        let test_cases = [
+            "a",
+            "1a",
+            "1+1",
+            "1=>-1",
+            "-1=>1",
+            "<<1",
+            "+1+1",
+        ];
+        
+        for test_case in &test_cases {
+            assert_eq!(test_case.parse::<Op>(), Err(()))
+        }
+    }
+}
